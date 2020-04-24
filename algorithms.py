1e331eb762632814f8c07e24931241ce6352f18c
pullmay <y.asunari.hikima@gmail.com>
2020-04-24 21:21:01 +0900

    modified

--- algorithms.py
+++ algorithms.py
@@ -294,3 +294,23 @@ def binarySearch(A, key):
 		else:
 			left = mid + 1
 	return False
+
+# Partition
+def partiton(A, p, r):
+	# partiton(A, 0, len(A)-1)
+	x = A[r]
+	i = p - 1
+	for j in range(p, r):
+		if A[j] <= x:
+			i += 1
+			A[i], A[j] = A[j], A[i]
+	A[i + 1], A[r] = A[r], A[i + 1]
+	return i + 1
+
+# Quick Sort O(NlogN)
+def quick_sort(A, p, r):
+	#  quick_sort(A, 0, len(A)-1)
+	if p < r:
+		q = partiton(A, p, r)
+		quick_sort(A, p, q - 1)
+		quick_sort(A, q + 1, r)
