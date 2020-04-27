514a5e0fd8369e3343822a5fb95f2a3ff23e39cc
pullmay <y.asunari.hikima@gmail.com>
2020-04-27 23:24:15 +0900

    add union find

--- algorithms.py
+++ algorithms.py
@@ -314,3 +314,20 @@ def quick_sort(A, p, r):
 		q = partiton(A, p, r)
 		quick_sort(A, p, q - 1)
 		quick_sort(A, q + 1, r)
+
+# fact mod
+def factorial_mod(n, mod):
+    a = 1
+    for i in range(1, n + 1):
+        a *= i
+        a %= mod
+    return a
+ 
+# combination mod
+def comb_mod(n, k, mod):
+    if k > n:
+        return 0
+    fact_n = factorial_mod(n, mod)
+    fact_k = factorial_mod(k, mod)
+    fact_n_k = factorial_mod(n - k, mod)
+    return (fact_n * pow(fact_k, mod - 2, mod) * pow(fact_n_k, mod - 2, mod)) % mod
