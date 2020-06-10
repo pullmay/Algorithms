a1f0ec5b7099e307246d30d1380b0ade97092baa
miruca <y.asunari.hikima@gmail.com>
2020-06-10 20:39:12 +0900

    t

--- Alg.py
+++ Alg.py
@@ -15,3 +15,16 @@ def prime_list(n):
 	for i in range(2, int(n ** 0.5) + 1):
 		primes.difference_update(range(i * 2, n + 1, i))
 	return list(primes)
+
+# エラトステネスの篩
+# nまでの整数([0,1,...,n])に対して、素数であるかのboolian配列
+def prime_eratosthenes(n):
+	primes = [True] * (n + 1)
+	p = 2
+	while (p * p <= n):
+		if primes[p]:
+			for i in range(p * p, n + 1, p):
+				primes[i] = False
+		p += 1
+	primes[0], primes[1] = False, False
+	return primes
