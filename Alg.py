fb27ebd9e2c19d3ad56f89e748861b4328c9c785
miruca <y.asunari.hikima@gmail.com>
2020-06-10 20:34:30 +0900

    tmp

--- Alg.py
+++ Alg.py
@@ -8,3 +8,10 @@ def is_power(n):
         if n % i == 0:
             return False
     return n != 1 # 1 のときは例外
+
+# nまでの素数リスト
+def prime_list(n):
+	primes = set(range(2, n + 1))
+	for i in range(2, int(n ** 0.5) + 1):
+		primes.difference_update(range(i * 2, n + 1, i))
+	return list(primes)
