f7e2fc78376061a812f9d72e9c992e1b6dfa20ef
miruca <y.asunari.hikima@gmail.com>
2020-06-10 20:55:15 +0900

    change tab space

--- itertools.py
+++ itertools.py
@@ -1,10 +1,10 @@
-import itertools
+from itertools import combinations, permutations
 
 # combinations
 # 第一引数: 第一引数にイテラブル(e.g., リストや集合set型)
 # 第二引数: 選択する個数
 lst = list(range(5)) # [0,1,2,3,4]
-c = itertools.combinations(lst, 2)
+c = combinations(lst, 2)
 
 print(c)
 # <itertools.combinations object at 0x000001CB30DD7DB8>
@@ -24,7 +24,7 @@ for v in c:
 # (3, 4)
 
 # リスト化可能
-print(list(itertools.combinations(lst, 2)))
+print(list(combinations(lst, 2)))
 # [(0, 1), (0, 2), (0, 3), (0, 4), (1, 2), (1, 3), (1, 4), (2, 3), (2, 4), (3, 4)]
 
 
@@ -32,7 +32,7 @@ print(list(itertools.combinations(lst, 2)))
 # 第一引数: 第一引数にイテラブル(e.g., リストや集合set型)
 # 第二引数: 選択する個数
 lst = [7, 8, 9]
-p = itertools.permutations(lst, len(lst))
+p = permutations(lst, len(lst))
 
 for v in p:
 	print(v)
@@ -45,5 +45,5 @@ for v in p:
 # (9, 8, 7)
 
 # リスト化可能
-print(list(itertools.permutations(lst, len(lst))))
+print(list(permutations(lst, len(lst))))
 # [(7, 8, 9), (7, 9, 8), (8, 7, 9), (8, 9, 7), (9, 7, 8), (9, 8, 7)]
